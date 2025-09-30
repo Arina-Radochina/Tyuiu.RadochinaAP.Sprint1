674dd08198e91631b783bac482a355cb88f9f778
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-09-30 22:14:27 +0300

    Task1

---
+++ Tyuiu.RadochinaAP.Sprint1.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RadochinaAP.Sprint1.Task1.V26.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint1.Task1.V26.Test
+{
+    public class DataServiceTest
+    {
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(5, res);
+        }
+    }
+}
