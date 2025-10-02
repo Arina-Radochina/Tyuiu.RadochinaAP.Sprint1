120000b8d2eddd71fcc234a3f369d6d4e47c07c9
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-02 11:47:07 +0300

    .

--- Tyuiu.RadochinaAP.Sprint1.Task5.V7.Lib/DataService.cs
+++ Tyuiu.RadochinaAP.Sprint1.Task5.V7.Lib/DataService.cs
@@ -6,7 +6,11 @@ namespace Tyuiu.RadochinaAP.Sprint1.Task5.V7.Lib
         public int AngleToHoursMinutes(double f)
         {
             int h = (int) (f / 30);
-            return (int) h;
+            if (h ==12)
+            {
+                h= 0;
+            }
+            return h;
             
         }
     }
